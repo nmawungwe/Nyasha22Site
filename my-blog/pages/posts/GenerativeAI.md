1f220ca9137bc0ede6429e88d070bfbd872b48f9
Nyasha <nmawungwe@gmail.com>
2023-06-10 15:02:50 +0200

    Worked on my previous projects page, and started looking into my post page. I will be a blog post due next week Friday LFG

---
+++ my-blog/pages/posts/GenerativeAI.md
@@ -0,0 +1,11 @@
+---
+title: Generative AI Hype of Fad
+date: 2023/6/10
+description: In this article I discuss whether the upcoming AI era is all hype or a fad.
+tag: AI
+author: Nyasha
+---
+
+### Is the AI era hype or fad ?
+
+Hallo readers this will be first article
--- my-blog/scripts/gen-rss.js
+++ my-blog/scripts/gen-rss.js
@@ -6,7 +6,7 @@ const matter = require('gray-matter')
 async function generate() {
   const feed = new RSS({
     title: 'Nyasha Mawungwe',
-    site_url: 'https://yoursite.com',
+    site_url: 'https://wwww.nyasha22.com',
     feed_url: 'https://yoursite.com/feed.xml',
   })
 
